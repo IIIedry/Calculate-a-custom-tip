eff3f219d04a9683696f2e7f06008920e6b3dcf2
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-10-27 23:47:04 +0600

    start project and add need function

--- Calculate_a_custom_tip/app/src/main/java/com/example/calculate_a_custom_tip/MainActivity.kt
+++ Calculate_a_custom_tip/app/src/main/java/com/example/calculate_a_custom_tip/MainActivity.kt
@@ -6,11 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculate_a_custom_tip.ui.theme.Calculate_a_custom_tipTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TipTimeLayout()
                 }
             }
         }
@@ -30,17 +30,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TipTimeLayout() {
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Calculate_a_custom_tipTheme {
-        Greeting("Android")
+        TipTimeLayout()
     }
 }
